70fa26eb38f74ffc85129aff50f0db12d0131761
Gabriel Chaves <gabrieloliveirachaves754@gmail.com>
2021-07-06 14:44:35 -0300

    ship movement implemented

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 let playerDirectionY, playerDirectionX;
+let game;
+let frame;
+let player, speedPlayer, playerPositionY, playerPositionX; // player control variables
+let screenWidth = window.innerWidth;
+let screenHeight = window.innerHeight;
 
 function keyDown(){
-    let key = KeyboardEvent.code;
-    if(key === 38){ // up
+    let key = event.keyCode;
+    if(key == 38){ // up
         playerDirectionY = -1;
-    }else if(key === 40){ // down
+    }else if(key == 40){ // down
         playerDirectionY = 1;
     }
-     if(key === 37){ // left
+     if(key == 37){ // left
         playerDirectionX = -1;
-    }else if(key === 39){ // right
+    }else if(key == 39){ // right
         playerDirectionX = 1;
     }
-    if(key === 32){ // shoot
+    if(key == 32){ // shoot
         // shoot
     }
 }
+
+
 function keyUp(){
-    let key = KeyboardEvent.code;
+    let key = event.keyCode;
     if((key == 38) || (key == 40)){
         playerDirectionY = 0;
     }
@@ -27,3 +34,39 @@ function keyUp(){
 
 }
 
+function playerControls(){
+    playerPositionY += playerDirectionY * speedPlayer;
+    playerPositionX += playerDirectionX * speedPlayer;
+    player.style.top = playerPositionY + 'px';
+    player.style.left = playerPositionX + 'px';
+}
+
+function gameLoop(){
+    if(game){
+        playerControls();
+
+    }
+    frame = requestAnimationFrame(gameLoop);
+}
+
+function start(){
+    game = true;
+
+    //initialazing player variables
+    playerDirectionX = playerDirectionY = 0;
+    playerPositionX = screenWidth / 2;
+    playerPositionY = screenHeight / 2;
+    speedPlayer = 5;
+    player = document.getElementById('play-ship');
+    player.style.top = playerPositionY + 'px';
+    player.style.left = playerPositionX + 'px';
+
+
+
+    gameLoop();
+}
+
+
+window.addEventListener('load', start);
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
